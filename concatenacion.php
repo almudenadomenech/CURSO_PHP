964f57617505da79452114cec1859c64069be7e9
almudenadomenech <violeta_33@hotmail.es>
2025-02-09 12:26:14 +0100

    concatenacion

---
+++ concatenacion.php
@@ -0,0 +1,7 @@
+<?php
+    $nombre = "Carlos";
+    $pais = "Turquia";
+
+    echo "Tu nombre es: " . $nombre . " " . "<br>" . " Vives en: " . $pais;
+
+    echo "Mi nombre es: {$nombre} mi país de residencia es {$pais}";
